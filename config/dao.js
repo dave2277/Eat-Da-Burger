d2984c2654064bea178a68cd746f97343d444fdf
dogray7722 <david.gray61@du.edu>
2017-08-21 16:25:52 -0600

    Renamed orm.js to dao.js because A DAO is an object that abstracts the implementation of a persistent data store away from the application and allows for simple interaction with it. An ORM is a robust library/API that provides a bunch of tools to save/retrieve an object directly to/from the database without having to write your own SQL statements.

---
+++ config/dao.js
@@ -0,0 +1,31 @@
+var connection = require("./connection");
+
+// var orm = {
+
+
+//     selectAll: function(tableInput, colToSearch, valOfCOl){
+//         var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+//         connection.query(queryString, [tableInput, colToSearch valOfCOl], function(err, result){
+//             console.log(result);
+//         });
+//     },
+//
+//     insertOne: function(tableInput, colToSearch, valOfCOl) {
+//         var queryString = "INSERT INTO ?? ";
+//         connection.query(queryString, [tableInput, colToSearch valOfCOl], function (err, result) {
+//             console.log(result);
+//
+//         });
+//     },
+//
+//     updateOne: function(tableInput, colToUpdate, newVal, valOfCOl) {
+//        var queryString = "UPDATE ?? SET ?? = ?? WHERE ?? = ?";
+//        connection.query(queryString, [tableInput, colToUpdate, valOfCOl], function (err, result){
+//
+//        })
+//     }
+// };
+
+
+
+// module.exports = orm;
