03b70e38bacda79981905e4008157b6e965b354d
dogray7722 <david.gray61@du.edu>
2017-08-22 23:31:36 -0600

    Model file is now exported.

--- models/burger.js
+++ models/burger.js
@@ -1,10 +1,17 @@
-var dao = require('../config/dao');
+const dao = require('../config/dao');
 
-var callback = function (err, data) {
+const callback = function (err, data) {
     console.log(data);
 };
 
-var data = dao.selectAll("id", "burger_name", "devoured", "burgers", callback);
+var burger = {
+
+    all: function (cb) {
+        dao.selectAll("id", "burger_name", "devoured", "burgers", callback);
 
 
 // dao.insertOne("burgers", "burger_name", "devoured", "date");
+    }
+};
+
+module.exports = burger;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var exphbs = require('express-handlebars');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
-var PORT = process.env.PORT || 8080;
+const express = require('express');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(express.static("public"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +19,9 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride('_method'));
 
 require('./controllers/burgers_controller')(app);
+
+//Took this line from the cats app.  Not exactly sure what it does or why it's necessary.  TAs?
+// app.use("/", routes);
 
 app.listen(PORT, function() {
     console.log('Listening on port ' + PORT);
